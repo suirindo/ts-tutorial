7a58ba9e5fb21ee4dd477b5650975e96ab7d5762
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-23 15:11:16 +0900

    0923

--- nextjs-gihyo-book/next.config.js
+++ nextjs-gihyo-book/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  compiler: {
+    // styledComponentsの有効化
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
